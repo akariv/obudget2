dd1faee9d458463a19aeaeab8ed765223eecc16c
gardenofwine <gardenofwine@gmail.com>
2012-04-23 22:43:50 +0300

    Add Facebook Connect integration

--- src/test-server/jsonp-server.js
+++ src/test-server/jsonp-server.js
@@ -1,25 +1,31 @@
-var http = require('http');
+var connect = require('connect')
+  , http = require('http');
+
 var url = require('url');
 
-var server = http.createServer(function(req, res) {
+var app = connect()
+	.use(connect.favicon())
+	.use(connect.logger('dev'))
+//	.use(connect.static('data'))
+	.use(function(req, res){
 
-	var url_parts = url.parse(req.url, true);
-	var query = url_parts.query;
-	var callback = query.callback;
+		var url_parts = url.parse(req.url, true);
+		var query = url_parts.query;
+		var callback = query.callback;
 
-	var data = null;
-	try {
-		data = require('.' + url_parts.pathname);
-		res.writeHead(200);
-		res.end(callback + "(" + JSON.stringify(data.data) + ")");
-	} catch (err) {
-		dumpError(err);
-		res.writeHead(500);
-	    res.end('Internal Server Error');
-	}
+		var data = null;
+		try {
+			data = require('.' + url_parts.pathname);
+			res.writeHead(200);
+			res.end(callback + "(" + JSON.stringify(data.data) + ")");
+		} catch (err) {
+			dumpError(err);
+			res.writeHead(500);
+		    res.end('Internal Server Error');
+		}
 
-});
-server.listen(8080);
+	});
+app.listen(8080);
 
 function dumpError(err) {
 	if (typeof err === 'object') {
