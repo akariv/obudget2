ba081281f6f76ff05a8bcddb474059322a009bc2
gardenofwine <gardenofwine@gmail.com>
2012-05-08 12:37:45 +0300

    Add Open Graph tags to index.html to enable gaceful facebook sharing

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express.createServer();
-app.get('/', function(req, res) {
-    res.send('Hello from Cloud Foundry');
-});
+//app.get('/', function(req, res) {
+//    res.send('Hello from Cloud Foundry');
+//});
 
 app.configure(function(){
-    app.use(express.static(__dirname + '/src/app/'));
-    }
-);
+	app.use(express.static(__dirname + '/src/app/'));
+});
 
 app.listen(process.env.VCAP_APP_PORT || 3000);
